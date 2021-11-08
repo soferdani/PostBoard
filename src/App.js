807fcebff68cd0217b46ee6c344b34d0b6c2a65d
soferdani <soferdani@gmail.com>
2021-11-08 22:07:42 +0200

    add bootsramp

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import "./App.css";
 import React, { useState } from "react";
+import {Button  } from "react-bootstrap";
 import Note from "./components/Note";
 const data = require("./assets/posts.json");
 
-
 const handleSubmit = (event) => {
-  console.log(event);
-}
-
+	console.log(event);
+};
 
 function App() {
 	const [notes, setNotes] = useState(data);
 	const [newAuthorName, setNewAuthorName] = useState("");
 	const [newNoteContent, setNewNoteContent] = useState("");
 
-  console.log(notes.posts)
+	console.log(notes.posts);
 
 	return (
 		<div className='App'>
 			<h1>Welcome to post board</h1>
 			<p>Here you can create and edit your posts</p>
-      
-      
-      
-      
-      <button>submit</button>
-      
-      {notes.posts.map((posts) => 
-        <Note key={posts.id} data={posts} />
-      )}
 
+      <Button variant="success">Success</Button>
+      
+			{notes.posts.map((posts) => (
+				<Note key={posts.id} data={posts} />
+			))}
 
 			{/* <form onSubmit={handleSubmit}>
 				<input
@@ -45,17 +40,12 @@ function App() {
 				/>
 				<button type='submit'>Add</button>
 			</form> */}
-
 		</div>
 	);
 }
 
-
-
-
 // 	event.target.title.value = ""; // resets the form
 // 	event.target.content.value = ""; // resets the form
 // }
-
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,17 @@
-
-
-
-
+import {Card } from "react-bootstrap";
 
 function Note(props) {
 
+
     return (
         <div className="note">
-            <h3>{props.data.content}</h3>
-            <h3>{props.data.author}</h3>
-            <h3>{props.data.date}</h3>
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                <Card.Title>{props.data.author}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{props.data.date}</Card.Subtitle>
+                <Card.Text>{props.data.content}</Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
